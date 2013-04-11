40eb6248e5ceee12e388f35df9261491c0a323dd
Jin Zhou <ferryzhou@gmail.com>
2013-04-11 19:52:26 -0400

    add desktop

---
+++ desktop.js
@@ -0,0 +1,18 @@
+host = 'http://localhost:3000';
+
+var card_tmpl = _.template($("#article_card").html());
+
+function render_cards(items) {
+  console.log('render cards ' + items.length);
+  var cards = [];
+  for (var i = 0; i < items.length; i++) {
+    cards.push(card_tmpl(items[i]));
+  }
+  console.log(cards.join('\n'));
+  $('#article_cards').html(cards.join('\n'));
+}
+
+
+$.getJSON(host + '/articles.json?callback=?', null, function(articles) {
+  render_cards(articles);
+});
